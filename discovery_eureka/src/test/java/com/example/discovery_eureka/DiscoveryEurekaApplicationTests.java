0cd953b18a0b7a5e39f6dd5eb76371313e5dbd02
DTD <tiendat30404@gmail.com>
2025-10-15 17:31:46 +0700

    config docker-compose

--- discovery_eureka/src/test/java/com/example/discovery_eureka/DiscoveryEurekaApplicationTests.java
+++ discovery_eureka/src/test/java/com/example/discovery_eureka/DiscoveryEurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.common_dtos;
+package com.example.discovery_eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CommonDtosApplicationTests {
+class DiscoveryEurekaApplicationTests {
 
 	@Test
 	void contextLoads() {
